8d5237c58ab47b7bf450708c613c2dcb05179382
Ajay Arjun <arjun.bka@gmail.com>
2017-08-09 15:02:09 -0400

    fix for multiple column level filters

--- app.py
+++ app.py
@@ -3,25 +3,27 @@ from bigparser import auth
 
 gridId = "57a34c80e4b017cc76c37c25"
 
-authId = auth.login("emailId", "password")
+# authId = auth.login("emailId", "password")
+
+authId = auth.login("arjun.bka@gmail.com", "AjayArjun")
 
 movies = grid(authId, gridId)
 
-headers = movies.getHeader()
-print(headers)
+# headers = movies.getHeader()
+# print(headers)
+#
+# rows = movies.getRows(searchfilter={'year': '2005'},columns=['film name ','language '])
+# print(rows)
 
-rows = movies.getRows(searchfilter={'year': '20005'},columns=['film name ','language '],sort=[{'film name ':'ASC'}])
+
+rows = movies.getRows(searchfilter={'GLOBAL': ['x-men'], 'year': ['2000','2016']},columns=['film name ', 'release date'])
 print(rows)
 
-
-rows = movies.getRows(searchfilter={'GLOBAL': ['x-men'], 'language ': 'english'}, sort=[{"year": "ASC"}],columns=['film name ', 'release date'])
-print(rows)
-
-result = movies.getLastRow(count=2, columns=['film name ', 'language '])
-print(result)
-
-result = movies.getRange(rows=5, columns=2)
-print(result)
-
-rows = movies.getRows(searchfilter={'GLOBAL': ['x-men'], 'language ': 'english'}, sort=[{"year": "ASC"}],columns=['film name ', 'release date'])
-print(rows)
+# result = movies.getLastRow(count=2, columns=['film name ', 'language '])
+# print(result)
+#
+# result = movies.getRange(rows=5, columns=2)
+# print(result)
+#
+# rows = movies.getRows(searchfilter={'GLOBAL': ['x-men'], 'language ': 'english'}, sort=[{"year": "ASC"}],columns=['film name ', 'release date'])
+# print(rows)
--- setup.py
+++ setup.py
@@ -2,17 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='bigparser',
-    version='0.0.4',
+    version='0.0.6',
     url='https://github.com/ajayarjun-bka/bigparser-python-lib',
     author='Ajay Arjun',
     author_email='arjun.bka@gmail.com',
-    description='Python client library for BigParser API.',
+    description='Python client library for BigParser\'s API to fetch data from grids.',
     packages=find_packages(exclude=['tests','app.py']),
     zip_safe=False,
     include_package_data=True,
-    platforms='any',
     license='MIT',
-    python_requires=">=3.3",
+    python_requires=">=3.1",
     install_requires=[
         'requests', 'pandas', 'numpy'
     ],
